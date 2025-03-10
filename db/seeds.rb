b1daa7281ad08fe18b2eaaaad4719b3b860f651d
tomyyn <tomasnaiouf@gmail.com>
2025-03-10 00:18:22 -0300

    Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@ TodoList.create(name: 'Setup Docker PG database')
 TodoList.create(name: 'Create todo_lists table')
 TodoList.create(name: 'Create TodoList model')
 TodoList.create(name: 'Create TodoList controller')
+
+TodoListItem.create(name: 'Create TodoListItems table', todo_list: TodoList.first)
+TodoListItem.create(name: 'Create TodoListItem model', todo_list: TodoList.first)
+TodoListItem.create(name: 'Create TodoListItem controller', todo_list: TodoList.first)
+TodoListItem.create(name: 'Create TodoListItem views', todo_list: TodoList.first)
